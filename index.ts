a71efdc42826dbfbbaeefb801f06b17028904d28
fdiengott <freddy.diengott@proton.me>
2024-04-30 09:19:15 -0400

    setup basic RAG implementation
    This is currently not setup with OpenAi's API key or another model just yet. I wanted to create the boilerplate first

--- index.ts
+++ index.ts
@@ -1,4 +1,18 @@
-async function main() {}
+import { Document, VectorStoreIndex } from "llamaindex";
+
+// INFO: this is a basic implementation of a vector data base. This file or others like it, might have more complicated/customized implementations
+async function main() {
+    const text = await Bun.file("./data/randy-nesse-interview.txt").text();
+
+    const document = new Document({ text });
+
+    const index = await VectorStoreIndex.fromDocuments([document]);
+
+    const queryEngine = index.asQueryEngine();
+    const query = "What does Randy primarily study?";
+    const response = await queryEngine.query({ query });
+
+    console.log(response.toString());
+}
 
 main();
-
