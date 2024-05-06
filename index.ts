d8857e7ba1910a13c14e7d8d9ca6ca1ac1637408
fdiengott <freddy.diengott@proton.me>
2024-05-06 01:10:01 -0400

    update RAG to use llama so it can run locally without any external APIs

--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
-import { Document, VectorStoreIndex } from "llamaindex";
+import {
+    Document,
+    Ollama,
+    OllamaEmbedding,
+    Settings,
+    VectorStoreIndex,
+} from "llamaindex";
 
-// INFO: this is a basic implementation of a vector data base. This file or others like it, might have more complicated/customized implementations
 async function main() {
     const text = await Bun.file("./data/randy-nesse-interview.txt").text();
 
-    const document = new Document({ text });
+    const customEmbedding = new OllamaEmbedding({
+        model: "nomic-embed-text",
+    });
+    const customLlm = new Ollama({ model: "llama2" });
 
+    Settings.llm = customLlm;
+    Settings.embedModel = customEmbedding;
+
+    const document = new Document({ text });
     const index = await VectorStoreIndex.fromDocuments([document]);
+
+    console.log("Indexed documents");
 
     const queryEngine = index.asQueryEngine();
     const query = "What does Randy primarily study?";
